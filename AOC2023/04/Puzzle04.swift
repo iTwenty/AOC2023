ddd35eed0c85bb26790d8e3e5d8781a21af8943d
jaydeep-helpshift <jaydeep@helpshift.com>
2023-12-19 15:52:39 +0530

    Solve day 4

--- AOC2023/04/Puzzle04.swift
+++ AOC2023/04/Puzzle04.swift
@@ -1,17 +1,43 @@
-//
-//  Puzzle04.swift
-//  AOC2023
-//
-//  Created by Jaydeep Joshi on 15/12/23.
-//
+struct Card: Hashable {
+    let id, winningCount: Int
+
+    init(id: Int, winningNumbers: Set<Int>, yourNumbers: Set<Int>) {
+        self.id = id
+        self.winningCount = yourNumbers.intersection(winningNumbers).count
+    }
+
+    static func parse(_ line: String) -> Self {
+        var split = line.components(separatedBy: ":")
+        let id = Int(split.first!.split(separator: " ").last!)!
+        split = split.last!.components(separatedBy: "|")
+        var (winningNumbers, yourNumbers) = (Set<Int>(), Set<Int>())
+        for str in split.first!.split(separator: " ") {
+            winningNumbers.insert(Int(str)!)
+        }
+        for str in split.last!.split(separator: " ") {
+            yourNumbers.insert(Int(str)!)
+        }
+        return Card(id: id, winningNumbers: winningNumbers, yourNumbers: yourNumbers)
+    }
+}
 
 struct Puzzle04: Puzzle {
+    private let cards = InputFileReader.read("Input04").map(Card.parse(_:))
 
     func part1() {
-        
+        let sum = cards.map { 1 << ($0.winningCount - 1) }.reduce(0, +)
+        print(sum)
     }
 
     func part2() {
-
+        var cardCounts = Array(repeating: 1, count: cards.count)
+        for index in cardCounts.indices {
+            let winningCount = cards[index].winningCount
+            if winningCount == 0 { continue }
+            for i in (index+1...index+winningCount) {
+                cardCounts[i] += cardCounts[index]
+            }
+        }
+        print(cardCounts.reduce(0, +))
     }
 }
