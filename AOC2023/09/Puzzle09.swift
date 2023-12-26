e1dc0e7f7b1a8dcfa6ed22425bc2e85512895800
jaydeep-helpshift <jaydeep@helpshift.com>
2023-12-26 18:01:14 +0530

    Solve day 9

--- AOC2023/09/Puzzle09.swift
+++ AOC2023/09/Puzzle09.swift
@@ -1,17 +1,25 @@
-//
-//  Puzzle09.swift
-//  AOC2023
-//
-//  Created by Jaydeep Joshi on 15/12/23.
-//
-
 struct Puzzle09: Puzzle {
+    private let histories = InputFileReader.read("Input09").map { line in
+        line.components(separatedBy: " ").compactMap(Int.init)
+    }
 
     func part1() {
-        
+        let extrapolatedValues = histories.map(extrapolatedValue(_:))
+        print(extrapolatedValues.reduce(0, +))
     }
 
     func part2() {
+        let extrapolatedValues = histories.map { extrapolatedValue($0.reversed()) }
+        print(extrapolatedValues.reduce(0, +))
+    }
 
+    private func extrapolatedValue(_ seq: [Int]) -> Int {
+        let triangle = sequence(first: seq) { input in
+            if input.allSatisfy({ $0 == 0 }) { return nil }
+            return zip(input.dropFirst(), input.dropLast()).map { (a, b) in
+                return a - b
+            }
+        }
+        return triangle.reduce(0) { $0 + $1.last! }
     }
 }
--- AOC2023/Runner.swift
+++ AOC2023/Runner.swift
@@ -10,7 +10,7 @@ import Foundation
 @main
 struct Runner {
     static func main() async throws {
-        let puzzle = Puzzle08()
+        let puzzle = Puzzle09()
         await puzzle.part1()
         await puzzle.part2()
     }
